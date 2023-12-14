ad19e9278bab3be69c0fb0edf027f3a00c3bbe89
Harin <master.harin@gmail.com>
2023-12-13 16:17:43 -0800

    Dungeon Map

--- src/main.rs
+++ src/main.rs
@@ -1,211 +1,62 @@
-use bracket_lib::prelude::*;
+mod camera;
+mod map;
+mod map_builder;
+mod player;
 
-enum GameMode {
-    Menu,
-    Playing,
-    End,
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+    pub use crate::map::*;
+    pub use crate::player::*;
+    pub use crate::map_builder::*;
+    pub use crate::camera::*;
 }
 
-const SCREEN_WIDTH: i32 = 80;
-const SCREEN_HEIGHT: i32 = 50;
-const FRAME_DURATION: f32 = 25.0;
+use prelude::*;
 
 struct State {
-    mode: GameMode,
-    frame_time: f32,
+    map: Map,
     player: Player,
-    obstacle: Obstacle,
-    score: i32,
-}
-
-struct Player {
-    x: i32,
-    y: i32,
-    velocity: f32,
-    momentum: f32,
-}
-
-struct Obstacle {
-    x: i32,
-    gap_y: i32,
-    size: i32,
-}
-
-impl Obstacle {
-    fn new(x: i32, score: i32) -> Self {
-        let mut random = RandomNumberGenerator::new();
-        Obstacle {
-            x,
-            gap_y: random.range(10, 40),
-            size: i32::max(2, 20 - (score * 2)),
-        }
-    }
-
-    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
-        let screen_x = self.x - player_x;
-        let half_size = self.size / 2;
-        // Draw the top half of the obstacle
-        for y in 0..self.gap_y - half_size {
-            ctx.set(
-                screen_x,
-                y,
-                RED,
-                BLACK,
-                to_cp437('|'),
-            );
-        }
-        // Draw the bottom half of the obstacle
-        for y in self.gap_y + half_size..SCREEN_HEIGHT {
-            ctx.set(
-                screen_x,
-                y,
-                RED,
-                BLACK,
-                to_cp437('|'),
-            );
-        }
-    }
-
-    fn hit_obstacle(&self, player: &Player) -> bool {
-        let half_size = self.size / 2;
-        let does_x_match = player.x == self.x;
-        let player_above_gap = player.y < self.gap_y - half_size;
-        let player_below_gap = player.y > self.gap_y + half_size;
-        does_x_match && (player_above_gap || player_below_gap)
-    }
-}
-
-impl Player {
-    fn new(x: i32, y: i32) -> Self {
-        Player {
-            x,
-            y,
-            velocity: 0.0,
-            momentum: 0.0,
-        }
-    }
-
-    fn render(&mut self, ctx: &mut BTerm) {
-        ctx.set(
-            0,
-            self.y,
-            YELLOW,
-            BLACK,
-            to_cp437('@'),
-        );
-    }
-
-    fn gravity_and_move(&mut self) {
-        if self.velocity < 2.0 {
-            self.velocity += 0.2 + self.momentum;
-            if self.momentum < 0.0 {
-                self.momentum += 0.2;
-            }
-        }
-
-        self.y += self.velocity as i32;
-        self.x += 1;
-        if self.y < 0 {
-            self.y = 0;
-        }
-    }
-
-    fn flap(&mut self) {
-        self.velocity = -2.0;
-        self.momentum = -0.5;
-    }
+    camera: Camera,
 }
 
 impl State {
     fn new() -> Self {
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
         Self {
-            mode: GameMode::Menu,
-            frame_time: 0.0,
-            player: Player::new(5, 25),
-            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
-            score: 0,
-        }
-    }
-
-    fn play(&mut self, ctx: &mut BTerm) {
-        ctx.cls_bg(NAVY);
-        self.frame_time += ctx.frame_time_ms;
-        if self.frame_time > FRAME_DURATION {
-            self.frame_time = 0.0;
-            self.player.gravity_and_move();
-        }
-        if let Some(VirtualKeyCode::Space) = ctx.key {
-            self.player.flap();
-        }
-        self.player.render(ctx);
-        ctx.print(0, 0, "Press SPACE to flap");
-        ctx.print(0, 1, &format!("Score: {}", self.score));
-        self.obstacle.render(ctx, self.player.x);
-        if self.player.x > self.obstacle.x {
-            self.score += 1;
-            self.obstacle = Obstacle::new(
-                self.player.x + SCREEN_WIDTH,
-                self.score,
-            );
-        }
-        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
-            self.mode = GameMode::End;
-        }
-    }
-
-    fn restart(&mut self) {
-        self.mode = GameMode::Playing;
-        self.player = Player::new(5, 25);
-        self.frame_time = 0.0;
-        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
-        self.score = 0;
-    }
-
-    fn main_menu(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print_centered(5, "Welcome to Flappy Dragon");
-        ctx.print_centered(8, "(P) Play Game");
-        ctx.print_centered(9, "(Q) Quit Game");
-
-        if let Some(key) = ctx.key {
-            match key {
-                VirtualKeyCode::P => self.restart(),
-                VirtualKeyCode::Q => ctx.quitting = true,
-                _ => {}
-            }
-        }
-    }
-
-    fn dead(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print_centered(5, "You are dead");
-        ctx.print_centered(6, &format!("You earned {} points", self.score));
-        ctx.print_centered(8, "(P) Play Again");
-        ctx.print_centered(9, "(Q) Quit Game");
-
-        if let Some(key) = ctx.key {
-            match key {
-                VirtualKeyCode::P => self.restart(),
-                VirtualKeyCode::Q => ctx.quitting = true,
-                _ => {}
-            }
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
+            camera: Camera::new(map_builder.player_start),
         }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        match self.mode {
-            GameMode::Menu => self.main_menu(ctx),
-            GameMode::Playing => self.play(ctx),
-            GameMode::End => self.dead(ctx),
-        }
+        ctx.set_active_console(0);
+        ctx.cls();
+        ctx.set_active_console(1);
+        ctx.cls();
+        self.player.update(ctx, &self.map, &mut self.camera);
+        self.map.render(ctx, &self.camera);
+        self.player.render(ctx, &self.camera);
     }
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
-        .with_title("Flappy Dragon")
+    let context = BTermBuilder::new()
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
     main_loop(context, State::new())
 }
