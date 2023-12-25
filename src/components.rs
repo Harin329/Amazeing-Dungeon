de7be8f3275682c9d1cc2ee4499e786e6b1f5db6
Harin <master.harin@gmail.com>
2023-12-24 21:35:45 -0800

    Field of View

--- src/components.rs
+++ src/components.rs
@@ -1,9 +1,10 @@
 pub use crate::prelude::*;
+use std::collections::HashSet;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Render {
     pub color: ColorPair,
-    pub glyph: FontCharType
+    pub glyph: FontCharType,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -18,13 +19,13 @@ pub struct MovingRandomly;
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct WantsToMove {
     pub entity: Entity,
-    pub destination: Point
+    pub destination: Point,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Health {
     pub current: i32,
-    pub max: i32
+    pub max: i32,
 }
 
 #[derive(Clone, PartialEq)]
@@ -33,7 +34,7 @@ pub struct Name(pub String);
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct WantsToAttack {
     pub attacker: Entity,
-    pub victim: Entity
+    pub victim: Entity,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -44,3 +45,28 @@ pub struct Item;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct AmuletOfYala;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct FieldOfView {
+    pub visible_tiles: HashSet<Point>,
+    pub radius: i32,
+    pub is_dirty: bool,
+}
+
+impl FieldOfView {
+    pub fn new(radius: i32) -> Self {
+        Self {
+            visible_tiles: HashSet::new(),
+            radius,
+            is_dirty: true,
+        }
+    }
+
+    pub fn clone_dirty(&self) -> Self {
+        Self {
+            visible_tiles: HashSet::new(),
+            radius: self.radius,
+            is_dirty: true,
+        }
+    }
+}
--- src/map.rs
+++ src/map.rs
@@ -9,12 +9,14 @@ pub enum TileType {
 
 pub struct Map {
     pub tiles: Vec<TileType>,
+    pub revealed_tiles: Vec<bool>,
 }
 
 impl Map {
     pub fn new() -> Self {
         Self {
             tiles: vec![TileType::Floor; NUM_TILES],
+            revealed_tiles: vec![false; NUM_TILES],
         }
     }
 
@@ -107,6 +109,10 @@ impl BaseMap for Map {
             exits.push((idx, 1.0));
         }
         exits
+    }
+
+    fn is_opaque(&self, idx: usize) -> bool {
+        self.tiles[idx] != TileType::Floor
     }
 
     fn get_pathing_distance(&self, idx1: usize, idx2: usize) -> f32 {
