70655a68dc8d124b652aaed6842a3376b6046583
Harin <master.harin@gmail.com>
2023-12-25 20:31:15 -0800

    Upload to Web

--- src/spawner/template.rs
+++ src/spawner/template.rs
@@ -1,7 +1,7 @@
 use crate::prelude::*;
 use serde::Deserialize;
-use ron::de::from_reader;
-use std::fs::File;
+// use ron::de::from_reader;
+// use std::fs::File;
 use std::collections::HashSet;
 
 #[derive(Clone, Deserialize, Debug, PartialEq)]
@@ -26,10 +26,13 @@ pub struct Templates {
     pub entities : Vec<Template>,
 }
 
+const TEMPLATE_FILE : &[u8] = include_bytes!("../../resources/template.ron");
+
 impl Templates {
     pub fn load() -> Self {
-        let file = File::open("resources/template.ron").expect("Failed opening file");
-        from_reader(file).expect("Unable to load templates")
+        ron::de::from_bytes(TEMPLATE_FILE).expect("Unable to load templates")
+        // let file = File::open("resources/template.ron").expect("Failed opening file");
+        // from_reader(file).expect("Unable to load templates")
     }
 
     pub fn spawn_entities(
